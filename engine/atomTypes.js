276887b26b914d1fc0107d412cabb1188da251fb
iammdtanvirrahman <mdtanvirrahman2022a@gmail.com>
2026-07-31 07:06:12 +0600

    Create engine/atomTypes.js

---
+++ engine/atomTypes.js
@@ -0,0 +1,27 @@
+export const ATOMS = {
+  H: {
+    valence: 1,
+    mass: 1,
+    color: '#8be9fd'
+  },
+
+  O: {
+    valence: 2,
+    mass: 16,
+    color: '#ff5555'
+  },
+
+  N: {
+    valence: 3,
+    mass: 14,
+    color: '#bd93f9'
+  },
+
+  C: {
+    valence: 4,
+    mass: 12,
+    color: '#50fa7b'
+  }
+};
+
+export const ATOM_KEYS = Object.keys(ATOMS);

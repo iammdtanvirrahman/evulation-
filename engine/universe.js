e5eb874f732625d155bd4c20a10f088d21f17b2f
iammdtanvirrahman <mdtanvirrahman2022a@gmail.com>
2026-07-31 07:10:11 +0600

    Update universe.js

--- engine/universe.js
+++ engine/universe.js
@@ -1,5 +1,6 @@
 import { Particle } from './particle.js';
 import { updatePhysics } from './physics.js';
+import { updateChemistry } from './chemistry.js';
 
 export class Universe {
   constructor(width, height, count = 600) {
@@ -19,9 +20,14 @@ export class Universe {
 
   update() {
     updatePhysics(this.particles, this.width, this.height);
+    updateChemistry(this.particles);
   }
 
   get totalEnergy() {
     return this.particles.reduce((s, p) => s + p.energy, 0);
+  }
+
+  get totalBonds() {
+    return this.particles.reduce((s, p) => s + p.bonds.size, 0) / 2;
   }
 }
